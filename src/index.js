ad7d99f1a045ecc7e3454cad2ef494e446c35951
Eleo <tahindrazaeleo@gmail.com>
2023-02-01 15:36:00 +0100

    fonction brique

--- src/index.js
+++ src/index.js
@@ -32,16 +32,73 @@ let getMousePos = (e) => {
 
 let draw = ()=>{
     ctx.clearRect(0,0,game_dim.w,game_dim.h)
+    
     ctx.strokeRect(0, 0, game_dim.w, game_dim.h)
-
+    
     ctx.fillStyle = 'black'
     ctx.fillText(`X : ${ev.x}, Y : ${ev.y}`, 100, 100)
 
+    
 
-    //dessin d'un rectagle rouge
-    ctx.fillStyle = 'red'
-    ctx.fillRect(0,0,100,100)
+    ctx.fillStyle='blue'
+    ctx.lineWidth = 1.5
+     //Brique
+    function brique(x, y , w, h , color ){
 
+        //initialisation brique
+        ctx.strokeRect(x,y,w,h)
+        ctx.strokeRect(x+5,y+5,w-10,h-10)
+
+        //all
+        ctx.fillStyle= color
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+        ctx.lineTo(x+w,y);
+        ctx.lineTo(x+w,y+h);
+        ctx.lineTo(x, y+h);
+        ctx.fill()
+
+        //face
+        ctx.fillStyle='rgba(0,0,0,0.35)'
+        ctx.beginPath();
+        ctx.moveTo(x+5,y+5);
+        ctx.lineTo(x+w-5,y+5);
+        ctx.lineTo(x+w-5,y+h-5);
+        ctx.lineTo(x+5, y+h-5);
+        ctx.fill()
+
+
+        //gauche
+        ctx.fillStyle= 'rgba(0,0,0,0.2)'   
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+        ctx.lineTo(x+5,y+5);
+        ctx.lineTo(x+5,y+h-5);
+        ctx.lineTo(x, y+h);
+        ctx.fill()
+
+        //droite
+        ctx.fillStyle='rgba(0,0,0,0.5)'
+        ctx.beginPath();
+        ctx.moveTo(x+w,y);
+        ctx.lineTo(x+w-5,y+5);
+        ctx.lineTo(x+w-5,y+h-5);
+        ctx.lineTo(x+w, y+h);
+        ctx.fill()
+
+        //bottom
+        ctx.fillStyle='rgba(0,0,0,0.7)'
+        ctx.beginPath();
+        ctx.moveTo(x,y+h);
+        ctx.lineTo(x+5,y+h-5);
+        ctx.lineTo(x+w-5,y+h-5);
+        ctx.lineTo(x+w, y+h);
+        ctx.fill()
+  
+    }
+
+    //test fonction brique
+    brique(100,150,80,40, 'white')
 }
 
 let update = ()=>{
